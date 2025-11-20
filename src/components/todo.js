e5c952c410fb01ecbb328d7e9020542f506886a7
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-11-20 12:23:23 +0100

    feat: add Todo component logic

---
+++ src/components/todo.js
@@ -0,0 +1,14 @@
+export class Todo {
+    constructor(title, description, dueDate, important = false, done = false) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.important = important;
+        this.done = done;
+    }
+    updateTitle (newTitle) {this.title = newTitle}
+    updateDescription (newDescription) {this.description = newDescription}
+    updateDueDate (newDueDate) {this.dueDate = newDueDate}
+    toggleImportant () {this.important = !this.important}
+    toggleDone () {this.done = !this.done}
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./style.css";
+
+import {Todo} from "./components/todo.js"
+
+const todo = new Todo("read book", "read Atomic habits", "21st november 2025");
